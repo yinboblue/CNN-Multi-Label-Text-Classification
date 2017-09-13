93568f76ab1e5227c5e2ee0a16f10fba9a80cdbd
RandolphVI <chinawolfman@hotmail.com>
2017-09-13 15:43:26 +0800

    Code refactoring.

--- CNN/data_helper.py
+++ CNN/data_helper.py
@@ -1,66 +1,212 @@
-import re
-import logging
+# -*- coding:utf-8 -*-
+
+import os
+import multiprocessing
 import numpy as np
-import pandas as pd
-from collections import Counter
+import gensim
+import logging
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as PathEffects
 
-def clean_str(s):
-	"""Clean sentence"""
-	s = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", s)
-	s = re.sub(r"\'s", " \'s", s)
-	s = re.sub(r"\'ve", " \'ve", s)
-	s = re.sub(r"n\'t", " n\'t", s)
-	s = re.sub(r"\'re", " \'re", s)
-	s = re.sub(r"\'d", " \'d", s)
-	s = re.sub(r"\'ll", " \'ll", s)
-	s = re.sub(r",", " , ", s)
-	s = re.sub(r"!", " ! ", s)
-	s = re.sub(r"\(", " \( ", s)
-	s = re.sub(r"\)", " \) ", s)
-	s = re.sub(r"\?", " \? ", s)
-	s = re.sub(r"\s{2,}", " ", s)
-	s = re.sub(r'\S*(x{2,}|X{2,})\S*',"xxx", s)
-	s = re.sub(r'[^\x00-\x7F]+', "", s)
-	return s.strip().lower()
+from pylab import mpl
+from gensim import corpora
+from gensim.models import word2vec
+from gensim.corpora import Dictionary
+from tflearn.data_utils import to_categorical, pad_sequences
+from sklearn.manifold import TSNE
 
-def load_data_and_labels(filename):
-	"""Load sentences and labels"""
-	df = pd.read_csv(filename, compression='zip', dtype={'consumer_complaint_narrative': object})
-	selected = ['product', 'consumer_complaint_narrative']
-	non_selected = list(set(df.columns) - set(selected))
+logging.getLogger().setLevel(logging.INFO)
 
-	df = df.drop(non_selected, axis=1) # Drop non selected columns
-	df = df.dropna(axis=0, how='any', subset=selected) # Drop null rows
-	df = df.reindex(np.random.permutation(df.index)) # Shuffle the dataframe
+mpl.rcParams['font.sans-serif'] = ['FangSong']  # 指定默认字体
+mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
 
-	# Map the actual labels to one hot labels
-	labels = sorted(list(set(df[selected[0]].tolist())))
-	one_hot = np.zeros((len(labels), len(labels)), int)
-	np.fill_diagonal(one_hot, 1)
-	label_dict = dict(zip(labels, one_hot))
+BASE_DIR = os.getcwd()
+TEXT_DIR = BASE_DIR + '/content.txt'
+VOCABULARY_DICT_DIR = BASE_DIR + '/math.dict'
+WORD2VEC_DIR = BASE_DIR + '/math.model'
 
-	x_raw = df[selected[1]].apply(lambda x: clean_str(x)).tolist()
-	y_raw = df[selected[0]].apply(lambda y: label_dict[y]).tolist()
-	return x_raw, y_raw, df, labels
+
+def create_vocab(text_file):
+    texts = []
+    with open(text_file, 'r') as fin:
+        for eachline in fin:
+            line = eachline.strip().split('\t')
+            texts.append(line)
+    vocab_dict = corpora.Dictionary(texts)
+    print(vocab_dict)
+    vocab_dict.save(VOCABULARY_DICT_DIR)
+    return vocab_dict
+
+
+# create_vocab(TEXT_DIR)
+
+def word2vec_train(embedding_size, input_file=TEXT_DIR, output_file=WORD2VEC_DIR):
+    sentences = word2vec.LineSentence(input_file)
+
+    # sg=0 -> CBOW model; sg=1 -> skip-gram model.
+    # 生成 embedding_size 的词向量 model
+    model = gensim.models.Word2Vec(sentences, size=embedding_size, min_count=0,
+                                   sg=0, workers=multiprocessing.cpu_count())
+    model.save(output_file)
+
+
+# word2vec_train(300)
+
+def data_word2vec(input_file, dictionary):
+    def token_to_index(content, dictionary):
+        result = []
+        for item in content:
+            if item != '<end>' and (len(item) > 0):
+                result.append(dictionary.token2id[item])
+        return result
+
+    with open(input_file) as fin:
+        labels = []
+        front_content_indexlist = []
+        behind_content_indexlist = []
+        for index, eachline in enumerate(fin):
+            front_content = []
+            behind_content = []
+            line = eachline.strip().split('\t')
+            label = line[2]
+            content = line[3].strip().split(' ')
+
+            end_tag = False
+            for item in content:
+                if item == '<end>':
+                    end_tag = True
+                if not end_tag:
+                    front_content.append(item)
+                if end_tag:
+                    behind_content.append(item)
+
+            labels.append(label)
+
+            front_content_indexlist.append(token_to_index(front_content, dictionary))
+            behind_content_indexlist.append(token_to_index(behind_content[1:], dictionary))
+        total_line = index + 1
+
+    class Data:
+        def __init__(self, total_line, labels, front_content_indexlist, behind_content_indexlist):
+            self.number = total_line
+            self.labels = labels
+            self.front_tokenindex = front_content_indexlist
+            self.behind_tokenindex = behind_content_indexlist
+
+    return Data(total_line, labels, front_content_indexlist, behind_content_indexlist)
+
+
+def load_word2vec_matrix(vocab_size, embedding_size):
+    model = gensim.models.Word2Vec.load(WORD2VEC_DIR)
+    vocab_dict = Dictionary.load(VOCABULARY_DICT_DIR)
+
+    vector = np.zeros([vocab_size, embedding_size])
+    for value, key in vocab_dict.items():
+        if len(key) > 0:
+            vector[value] = model[key]
+
+    return vector
+
+
+def max_seq_len_cal(content_indexlist):
+    result = 0
+    for item in content_indexlist:
+        if len(item) > result:
+            result = len(item)
+    return result
+
+
+def load_data_and_labels(data_file, MAX_SEQUENCE_LENGTH, EMBEDDING_SIZE):
+    """
+    Loads research data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load vocabulary dict file
+    vocab_dict = create_vocab(TEXT_DIR)
+
+    # Load word2vec file
+    word2vec_train(EMBEDDING_SIZE, TEXT_DIR, WORD2VEC_DIR)
+
+    # Load data from files and split by words
+    data = data_word2vec(input_file=data_file, dictionary=vocab_dict)
+    max_seq_len = max(max_seq_len_cal(data.front_tokenindex), max_seq_len_cal(data.behind_tokenindex))
+    logging.info('Found %s texts.' % data.number)
+    logging.info('Max sequence length is:', max_seq_len)
+    data_front = pad_sequences(data.front_tokenindex, maxlen=MAX_SEQUENCE_LENGTH, value=0.)
+    data_behind = pad_sequences(data.behind_tokenindex, maxlen=MAX_SEQUENCE_LENGTH, value=0.)
+    labels = to_categorical(data.labels, nb_classes=2)
+    logging.info('Shape of data front tensor:', data_front.shape)
+    logging.info('Shape of data behind tensor:', data_behind.shape)
+    logging.info('Shape of label tensor:', labels.shape)
+    return data_front, data_behind, labels
+
+
+def load_vocab_size(vocab_data_file=VOCABULARY_DICT_DIR):
+    vocab_dict = Dictionary.load(vocab_data_file)
+    return len(vocab_dict.items())
+
+
+# pretrained_word2vec_matrix = load_word2vec_matrix(load_vocab_size(), embedding_size=128)
+# print(pretrained_word2vec_matrix)
+# print(type(pretrained_word2vec_matrix))
+# print(pretrained_word2vec_matrix.shape)
+
+def plot_word2vec():
+    model = gensim.models.Word2Vec.load(WORD2VEC_DIR)
+    data_x = []
+    data_y = []
+    for index, item in enumerate(model.wv.vocab):
+        data_x.append(model[item])
+        data_y.append(item)
+    tsne = TSNE(n_components=2)
+    x_tsne = tsne.fit_transform(data_x)
+
+    def scatter(x, y):
+        f = plt.figure(figsize=(50, 50))
+        ax = plt.subplot(aspect='equal')
+        sc = ax.scatter(x[:, 0], x[:, 1], lw=0, s=40)
+        plt.xlim(-25, 25)
+        plt.ylim(-25, 25)
+        ax.axis('off')
+        ax.axis('tight')
+        txts = []
+        for i in range(len(y)):
+            # Position of each label.
+            if i % 20 == 0:
+                txt = ax.text(x[i, 0], x[i, 1], y[i], fontsize=10)
+                txt.set_path_effects([
+                    PathEffects.Stroke(linewidth=5, foreground="w"),
+                    PathEffects.Normal()])
+                txts.append(txt)
+        return f, ax, sc, txts
+
+    scatter(x_tsne, data_y)
+    plt.savefig('word_vector.png', dpi=150)
+
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
-	"""Iterate the data batch by batch"""
-	data = np.array(data)
-	data_size = len(data)
-	num_batches_per_epoch = int(data_size / batch_size) + 1
-
-	for epoch in range(num_epochs):
-		if shuffle:
-			shuffle_indices = np.random.permutation(np.arange(data_size))
-			shuffled_data = data[shuffle_indices]
-		else:
-			shuffled_data = data
-
-		for batch_num in range(num_batches_per_epoch):
-			start_index = batch_num * batch_size
-			end_index = min((batch_num + 1) * batch_size, data_size)
-			yield shuffled_data[start_index:end_index]
-
-if __name__ == '__main__':
-	input_file = './data/consumer_complaints.csv.zip'
-	load_data_and_labels(input_file)
+    """
+    含有 yield 说明不是一个普通函数，是一个 Generator.
+    函数效果：对 data，一共分成 num_epochs 个阶段（epoch），在每个 epoch 内，如果 shuffle=True，就将 data 重新洗牌，
+    批量生成 (yield) 一批一批的重洗过的data，每批大小是 batch_size，一共生成 int(len(data)/batch_size)+1 批。
+    Generate a  batch iterator for a dataset.
+    :param data: The data
+    :param batch_size: The size of the data batch
+    :param num_epochs: The number of epoches
+    :param shuffle: Shuffle or not
+    :return:
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((len(data) - 1) / batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
--- CNN/text_cnn.py
+++ CNN/text_cnn.py
@@ -1,80 +1,151 @@
-import numpy as np
+# -*- coding:utf-8 -*-
+
 import tensorflow as tf
 
+
+def linear(input_, output_size, scope=None):
+    """
+    Linear map: output[k] = sum_i(Matrix[k, i] * args[i] ) + Bias[k]
+    :param input_: a tensor or a list of 2D, batch x n, Tensors.
+    :param output_size: int, second dimension of W[i].
+    :param scope: VariableScope for the created subgraph; defaults to "Linear".
+    :returns: A 2D Tensor with shape [batch x output_size] equal to \
+                sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+    :raises: ValueError, if some of the arguments has unspecified or wrong shape.
+    """
+
+    shape = input_.get_shape().as_list()
+    if len(shape) != 2:
+        raise ValueError("Linear is expecting 2D arguments: %s" % str(shape))
+    if not shape[1]:
+        raise ValueError("Linear expects shape[1] of arguments: %s" % str(shape))
+    input_size = shape[1]
+
+    # Now the computation.
+    with tf.variable_scope(scope or "SimpleLinear"):
+        matrix = tf.get_variable("Matrix", [output_size, input_size], dtype=input_.dtype)
+        bias_term = tf.get_variable("Bias", [output_size], dtype=input_.dtype)
+
+    return tf.matmul(input_, tf.transpose(matrix)) + bias_term
+
+
+def highway(input_, size, num_layers=1, bias=-2.0, f=tf.nn.relu, scope='Highway'):
+    """
+    Highway Network (cf. http://arxiv.org/abs/1505.00387).
+    t = sigmoid(Wy + b)
+    z = t * g(Wy + b) + (1 - t) * y
+    where g is nonlinearity, t is transform gate, and (1 - t) is carry gate.
+    """
+
+    with tf.variable_scope(scope):
+        for idx in range(num_layers):
+            g = f(linear(input_, size, scope='highway_lin_%d' % idx))
+            t = tf.sigmoid(linear(input_, size, scope='highway_gate_%d' % idx) + bias)
+            output = t * g + (1. - t) * input_
+            input_ = output
+
+    return output
+
+
 class TextCNN(object):
-	def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0):
-		# Placeholders for input, output and dropout
-		self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name='input_x')
-		self.input_y = tf.placeholder(tf.float32, [None, num_classes], name='input_y')
-		self.dropout_keep_prob = tf.placeholder(tf.float32, name='dropout_keep_prob')
+    """
+    A CNN for text classification.
+    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
 
-		# Keeping track of l2 regularization loss (optional)
-		l2_loss = tf.constant(0.0)
+    def __init__(
+            self, sequence_length, num_classes, vocab_size,
+            embedding_size, filter_sizes, num_filters, l2_reg_lambda=0.0, pretrained_embedding=None):
 
-		# Embedding layer
-		with tf.device('/cpu:0'), tf.name_scope('embedding'):
-			W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name='W')
-			self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-			self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
+        # Placeholders for input, output and dropout
+        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
+        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
+        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
-		# Create a convolution + maxpool layer for each filter size
-		pooled_outputs = []
-		for i, filter_size in enumerate(filter_sizes):
-			with tf.name_scope('conv-maxpool-%s' % filter_size):
-				# Convolution Layer
-				filter_shape = [filter_size, embedding_size, 1, num_filters]
-				W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
-				b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name='b')
-				conv = tf.nn.conv2d(
-					self.embedded_chars_expanded,
-					W,
-					strides=[1, 1, 1, 1],
-					padding='VALID',
-					name='conv')
+        # Keeping track of l2 regularization loss (optional)
+        l2_loss = tf.constant(0.0)
 
-				# Apply nonlinearity
-				h = tf.nn.relu(tf.nn.bias_add(conv, b), name='relu')
+        # Vector = load_word2vec(TEXT_DATA_DIR, WORD2VEC_DIR, DICTIONARY_DIR, vocab_size, embedding_size)
 
-				# Maxpooling over the outputs
-				pooled = tf.nn.max_pool(
-					h,
-					ksize=[1, sequence_length - filter_size + 1, 1, 1],
-					strides=[1, 1, 1, 1],
-					padding='VALID',
-					name='pool')
-				pooled_outputs.append(pooled)
+        # Embedding layer
+        with tf.device('/cpu:0'), tf.name_scope("embedding"):
+            # 默认采用的是随机生成正态分布的词向量。
+            # 也可以是通过自己的语料库训练而得到的词向量。
+            if pretrained_embedding is None:
+                self.W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
+            else:
+                self.W = tf.Variable(pretrained_embedding, name="W", trainable=True)
+                self.W = tf.cast(self.W, tf.float32)
+            self.embedded_chars = tf.nn.embedding_lookup(self.W, self.input_x_front)
+            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
-		# Combine all the pooled features
-		num_filters_total = num_filters * len(filter_sizes)
-		self.h_pool = tf.concat(pooled_outputs,3)
-		self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+        # Create a convolution + maxpool layer for each filter size
+        pooled_outputs = []
 
-		# Add dropout
-		with tf.name_scope('dropout'):
-			self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
+        for i, filter_size in enumerate(filter_sizes):
+            with tf.name_scope("conv-maxpool-%s".format(filter_size)):
+                # Convolution Layer
+                filter_shape = [filter_size, embedding_size, 1, num_filters]
+                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
+                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
+                conv_front = tf.nn.conv2d(
+                    self.embedded_chars_expanded,
+                    W,
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                    name="conv")
 
-		# Final (unnormalized) scores and predictions
-		with tf.name_scope('output'):
-			W = tf.get_variable(
-				'W',
-				shape=[num_filters_total, num_classes],
-				initializer=tf.contrib.layers.xavier_initializer())
-			b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name='b')
-			l2_loss += tf.nn.l2_loss(W)
-			l2_loss += tf.nn.l2_loss(b)
-			self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name='scores')
-			self.predictions = tf.argmax(self.scores, 1, name='predictions')
+                # Apply nonlinearity
+                h_front = tf.nn.relu(tf.nn.bias_add(conv_front, b), name="relu_front")
 
-		# Calculate mean cross-entropy loss
-		with tf.name_scope('loss'):
-			losses = tf.nn.softmax_cross_entropy_with_logits(labels = self.input_y, logits = self.scores) #  only named arguments accepted            
-			self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+                # Maxpooling over the outputs
+                pooled = tf.nn.max_pool(
+                    h_front,
+                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding="VALID",
+                    name="pool_front")
 
-		# Accuracy
-		with tf.name_scope('accuracy'):
-			correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-			self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, 'float'), name='accuracy')
+                pooled_outputs.append(pooled)
 
-		with tf.name_scope('num_correct'):
-			correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-			self.num_correct = tf.reduce_sum(tf.cast(correct_predictions, 'float'), name='num_correct')
+        # Combine all the pooled features
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(pooled_outputs, 3)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
+        # Add highway
+        with tf.name_scope("highway"):
+            self.h_highway = highway(self.h_pool_flat, self.h_pool_flat.get_shape()[1], 1, 0)
+
+        # Add dropout
+        with tf.name_scope("dropout"):
+            self.h_drop = tf.nn.dropout(self.h_highway, self.dropout_keep_prob)
+
+        # Final (unnormalized) scores and predictions
+        with tf.name_scope("output"):
+            W = tf.get_variable(
+                "W",
+                shape=[num_filters_total * 2, num_classes],
+                initializer=tf.contrib.layers.xavier_initializer())
+            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
+            l2_loss += tf.nn.l2_loss(W)
+            l2_loss += tf.nn.l2_loss(b)
+            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
+            self.softmaxScores = tf.nn.softmax(self.scores, name="softmaxScores")
+            self.sigmoidScores = tf.nn.sigmoid(self.scores, name="sigmoidScores")
+            self.predictions = tf.argmax(self.scores, 1, name="predictions")
+            self.topKPreds = tf.nn.top_k(self.softmaxScores, k=1, sorted=True, name="topKPreds")
+
+        # CalculateMean cross-entropy loss
+        with tf.name_scope("loss"):
+            losses = tf.nn.softmax_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
+            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
+
+        # Accuracy
+        with tf.name_scope("accuracy"):
+            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
+            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
+
+        # AUC
+        with tf.name_scope("AUC"):
+            self.AUC = tf.contrib.metrics.streaming_auc(self.softmaxScores, self.input_y)
