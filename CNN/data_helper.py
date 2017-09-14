e5fb18aad7e40ab84304306e2fe14fd03216f4f8
RandolphVI <chinawolfman@hotmail.com>
2017-09-14 17:15:28 +0800

    Code refactoring.

--- CNN/data_helper.py
+++ CNN/data_helper.py
@@ -5,13 +5,12 @@ import multiprocessing
 import numpy as np
 import gensim
 import logging
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patheffects as PathEffects
 
 from pylab import mpl
-from gensim import corpora
 from gensim.models import word2vec
-from gensim.corpora import Dictionary
 from tflearn.data_utils import to_categorical, pad_sequences
 from sklearn.manifold import TSNE
 
@@ -22,42 +21,43 @@ mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显�
 
 BASE_DIR = os.getcwd()
 TEXT_DIR = BASE_DIR + '/content.txt'
-VOCABULARY_DICT_DIR = BASE_DIR + '/math.dict'
-WORD2VEC_DIR = BASE_DIR + '/math.model'
 
 
-def create_vocab(text_file):
-    texts = []
-    with open(text_file, 'r') as fin:
-        for eachline in fin:
-            line = eachline.strip().split('\t')
-            texts.append(line)
-    vocab_dict = corpora.Dictionary(texts)
-    print(vocab_dict)
-    vocab_dict.save(VOCABULARY_DICT_DIR)
-    return vocab_dict
+def create_word2vec_model(embedding_size, input_file=TEXT_DIR):
+    word2vec_file = BASE_DIR + '/word2vec_' + str(embedding_size) + '.model'
+
+    if os.path.isfile(word2vec_file):
+        logging.info('☛ The word2vec model you want create already exists!')
+    else:
+        sentences = word2vec.LineSentence(input_file)
+        # sg=0 means use CBOW model(default); sg=1 means use skip-gram model.
+        model = gensim.models.Word2Vec(sentences, size=embedding_size, min_count=0,
+                                       sg=0, workers=multiprocessing.cpu_count())
+        model.save(word2vec_file)
 
 
-# create_vocab(TEXT_DIR)
+def load_vocab_size(embedding_size):
+    word2vec_file = BASE_DIR + '/word2vec_' + str(embedding_size) + '.model'
 
-def word2vec_train(embedding_size, input_file=TEXT_DIR, output_file=WORD2VEC_DIR):
-    sentences = word2vec.LineSentence(input_file)
-
-    # sg=0 -> CBOW model; sg=1 -> skip-gram model.
-    # 生成 embedding_size 的词向量 model
-    model = gensim.models.Word2Vec(sentences, size=embedding_size, min_count=0,
-                                   sg=0, workers=multiprocessing.cpu_count())
-    model.save(output_file)
+    if os.path.isfile(word2vec_file):
+        model = word2vec.Word2Vec.load(word2vec_file)
+        return len(model.wv.vocab.items())
+    else:
+        logging.info("✘ The word2vec file doesn't exist. "
+                     "Please use function <create_vocab_size(embedding_size)> to create it!")
 
 
-# word2vec_train(300)
+def data_word2vec(input_file, word2vec_model):
+    vocab = dict([(k, v.index) for (k, v) in word2vec_model.wv.vocab.items()])
 
-def data_word2vec(input_file, dictionary):
-    def token_to_index(content, dictionary):
+    def token_to_index(content):
         result = []
         for item in content:
             if item != '<end>' and (len(item) > 0):
-                result.append(dictionary.token2id[item])
+                id = vocab.get(item)
+                if id is None:
+                    id = 0
+                result.append(id)
         return result
 
     with open(input_file) as fin:
@@ -82,77 +82,80 @@ def data_word2vec(input_file, dictionary):
 
             labels.append(label)
 
-            front_content_indexlist.append(token_to_index(front_content, dictionary))
-            behind_content_indexlist.append(token_to_index(behind_content[1:], dictionary))
+            front_content_indexlist.append(token_to_index(front_content))
+            behind_content_indexlist.append(token_to_index(behind_content[1:]))
         total_line = index + 1
 
     class Data:
-        def __init__(self, total_line, labels, front_content_indexlist, behind_content_indexlist):
-            self.number = total_line
-            self.labels = labels
-            self.front_tokenindex = front_content_indexlist
-            self.behind_tokenindex = behind_content_indexlist
+        def __init__(self):
+            pass
 
-    return Data(total_line, labels, front_content_indexlist, behind_content_indexlist)
+        @property
+        def number(self):
+            return total_line
+
+        @property
+        def labels(self):
+            return labels
+
+        @property
+        def front_tokenindex(self):
+            return front_content_indexlist
+
+        @property
+        def behind_tokenindex(self):
+            return behind_content_indexlist
+
+    return Data()
 
 
 def load_word2vec_matrix(vocab_size, embedding_size):
-    model = gensim.models.Word2Vec.load(WORD2VEC_DIR)
-    vocab_dict = Dictionary.load(VOCABULARY_DICT_DIR)
+    word2vec_file = BASE_DIR + '/word2vec_' + str(embedding_size) + '.model'
 
-    vector = np.zeros([vocab_size, embedding_size])
-    for value, key in vocab_dict.items():
-        if len(key) > 0:
-            vector[value] = model[key]
-
-    return vector
-
-
-def max_seq_len_cal(content_indexlist):
-    result = 0
-    for item in content_indexlist:
-        if len(item) > result:
-            result = len(item)
-    return result
+    if os.path.isfile(word2vec_file):
+        model = gensim.models.Word2Vec.load(word2vec_file)
+        vocab = dict([(k, v.index) for k, v in model.wv.vocab.items()])
+        vector = np.zeros([vocab_size, embedding_size])
+        for key, value in vocab.items():
+            if len(key) > 0:
+                vector[value] = model[key]
+        return vector
+    else:
+        logging.info("✘ The word2vec file doesn't exist. "
+                     "Please use function <create_vocab_size(embedding_size)> to create it!")
 
 
-def load_data_and_labels(data_file, MAX_SEQUENCE_LENGTH, EMBEDDING_SIZE):
+def load_data_and_labels(data_file, embedding_size):
     """
     Loads research data from files, splits the data into words and generates labels.
     Returns split sentences and labels.
     """
-    # Load vocabulary dict file
-    vocab_dict = create_vocab(TEXT_DIR)
+    word2vec_file = BASE_DIR + '/word2vec_' + str(embedding_size) + '.model'
 
-    # Load word2vec file
-    word2vec_train(EMBEDDING_SIZE, TEXT_DIR, WORD2VEC_DIR)
+    # Load word2vec model file
+    if os.path.isfile(word2vec_file):
+        model = word2vec.Word2Vec.load(word2vec_file)
+    else:
+        create_word2vec_model(embedding_size, TEXT_DIR)
 
     # Load data from files and split by words
-    data = data_word2vec(input_file=data_file, dictionary=vocab_dict)
-    max_seq_len = max(max_seq_len_cal(data.front_tokenindex), max_seq_len_cal(data.behind_tokenindex))
-    logging.info('Found %s texts.' % data.number)
-    logging.info('Max sequence length is:', max_seq_len)
-    data_front = pad_sequences(data.front_tokenindex, maxlen=MAX_SEQUENCE_LENGTH, value=0.)
-    data_behind = pad_sequences(data.behind_tokenindex, maxlen=MAX_SEQUENCE_LENGTH, value=0.)
+    data = data_word2vec(input_file=data_file, word2vec_model=model)
+    max_front_len = max([len(x) for x in data.front_tokenindex])
+    max_behind_len = max([len(x) for x in data.behind_tokenindex])
+    max_seq_len = max(max_front_len, max_behind_len)
+    logging.info('Found {} texts.'.format(data.number))
+    return data, max_seq_len
+
+
+def pad_data(data, max_seq_len):
+    data_front = pad_sequences(data.front_tokenindex, maxlen=max_seq_len, value=0.)
+    data_behind = pad_sequences(data.behind_tokenindex, maxlen=max_seq_len, value=0.)
     labels = to_categorical(data.labels, nb_classes=2)
-    logging.info('Shape of data front tensor:', data_front.shape)
-    logging.info('Shape of data behind tensor:', data_behind.shape)
-    logging.info('Shape of label tensor:', labels.shape)
     return data_front, data_behind, labels
 
 
-def load_vocab_size(vocab_data_file=VOCABULARY_DICT_DIR):
-    vocab_dict = Dictionary.load(vocab_data_file)
-    return len(vocab_dict.items())
-
-
-# pretrained_word2vec_matrix = load_word2vec_matrix(load_vocab_size(), embedding_size=128)
-# print(pretrained_word2vec_matrix)
-# print(type(pretrained_word2vec_matrix))
-# print(pretrained_word2vec_matrix.shape)
-
-def plot_word2vec():
-    model = gensim.models.Word2Vec.load(WORD2VEC_DIR)
+def plot_word2vec(word2vec_file):
+    model = gensim.models.Word2Vec.load(word2vec_file)
     data_x = []
     data_y = []
     for index, item in enumerate(model.wv.vocab):
